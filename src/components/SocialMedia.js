d3c184b7620476d70c6df3c898e15d0c4019ceda
jchurley95 <jchurley95@gmail.com>
2017-08-07 09:42:16 -0400

    Updated social media links, removed Facebook link

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import fbook from '../Images/Facebook_icon.png';
 import ghub from '../Images/github_icon.png';
 import insta from '../Images/instagram_icon.png';
 import linked from '../Images/linkedin_icon.png';
@@ -8,10 +7,9 @@ class SocialMedia extends Component {
     render () {
         return (
             <div className="social-media-icons-container">
-                <a href="http://www.facebook.com/vivalajoey" target="_blank"><img className="icon facebook-icon" src={fbook} /></a>
-                <a href="http://www.facebook.com/vivalajoey" target="_blank"><img className="icon github-icon" src={ghub} /></a>
-                <a href="http://www.facebook.com/vivalajoey" target="_blank"><img className="icon instagram-icon" src={insta} /></a>
-                <a href="http://www.facebook.com/vivalajoey" target="_blank"><img className="icon linkedin-icon" src={linked} /></a>
+                <a href="https://github.com/jchurley95" target="_blank"><img className="icon github-icon" src={ghub} /></a>
+                <a href="https://www.instagram.com/screwloosecarpentry/" target="_blank"><img className="icon instagram-icon" src={insta} /></a>
+                <a href="https://www.linkedin.com/in/joey-hurley/" target="_blank"><img className="icon linkedin-icon" src={linked} /></a>
             </div>
         );
     }
